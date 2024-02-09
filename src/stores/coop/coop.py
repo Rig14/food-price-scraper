9591d4fe0af15d40eebcd140aa44e75c013dcdc3
ririvi <ririvi@taltech.ee>
2024-02-09 14:35:05 +0200

    Add new dependencies and implement category retrieval in Coop store

--- src/stores/coop/coop.py
+++ src/stores/coop/coop.py
@@ -1,5 +1,7 @@
 """Coop Store scraper."""
 
+import requests
+
 from src.util.item import Item
 from src.util.store import Store
 
@@ -15,3 +17,16 @@ class Coop(Store):
         return [
             Item("Milk", "123", 199, "Dairy", 10),
         ]
+
+    def _get_categories(self) -> dict[str, int]:
+        """Get category names and their respective ids."""
+        url = "https://api.vandra.ecoop.ee/supermarket/categories/nested?language=et"
+
+        response = requests.get(url)
+
+        data = response.json().get("data")
+
+        # keep only categories that are below id 70 (only food categories)
+        categories = {x["name"]: x["id"] for x in data if x["id"] < 70}
+
+        return categories
--- src/stores/coop/tests/test_coop.py
+++ src/stores/coop/tests/test_coop.py
@@ -1,14 +1,28 @@
 """Coop store scraper tests."""
 
 from src.stores.coop.coop import Coop
-from src.util.item import Item
 
 
 coop = Coop()
 
 
-def test_get_items_has_items():
-    """Test get_items method returns something."""
-    items = coop.get_items()
-    assert isinstance(items[0], Item)
-    assert len(items) > 0
+def test_get_categories_has_categories():
+    """Test get_categories method returns something."""
+    categories = coop._get_categories()
+    assert isinstance(categories, dict)
+    assert len(categories) > 0
+
+
+def test_get_categories_has_known_categories_3_tests():
+    """Test get_categories method returns known categories."""
+    categories = coop._get_categories()
+
+    known = {
+        "Puu- ja köögiviljad": 1,
+        "Pagaritooted": 47,
+        "Piimatooted, munad, või": 20,
+    }
+
+    for name, _id in known.items():
+        assert name in categories
+        assert categories[name] == _id
