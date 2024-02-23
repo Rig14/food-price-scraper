61d7032299afdde4301f89bae31ae3619ac1e9cd
ririvi <ririvi@taltech.ee>
2024-02-23 18:26:36 +0200

    Refactor Selver store scraper to improve performance and add testing capabilities

--- src/stores/selver/selver.py
+++ src/stores/selver/selver.py
@@ -1,10 +1,10 @@
 """Selver store scraper."""
 
+import random
 import requests
 from src.util.item import Item
 from src.util.store import Store
-
-PRODUCT_URL = 'https://www.selver.ee/api/catalog/vue_storefront_catalog_et/product/_search?request={"query":{"bool":{"filter":{"bool":{"must":[{"terms":{"visibility":[2,3,4]}},{"terms":{"status":[0,1]}},{"terms":{"category_ids":[209,210,212,213,214,215,216,217,369]}}]}}}}}&size=24'
+from src.stores.selver.helper import construct_request
 
 
 class Selver(Store):
@@ -13,9 +13,32 @@ class Selver(Store):
     def __init__(self):
         self.name = "Selver"
 
-    def get_items(self) -> list[Item]:
-        """Returns all the food items Selver web store has."""
-        pass
+    def get_items(self, _test: bool = False) -> list[Item]:
+        """
+        Returns all the food items Selver web store has.
+
+        :param _test: bool: if True, the method will return a list of items for testing purposes (smaller request size)
+        """
+        items: list[Item] = []
+        category_ids = self._get_ids()
+
+        # split category_ids into chunks of 10
+        chunks = []
+        for i in range(0, len(category_ids), 10):
+            try:
+                chunks.append(category_ids[i : i + 10])
+            except IndexError:
+                chunks.append(category_ids[i:])
+
+        # if testing is enabled, only get one random chunk
+        if _test:
+            chunks = [chunks[random.randint(0, len(chunks) - 1)]]
+
+        # get items from each chunk (10 categoryes at a time)
+        for chunk in chunks:
+            items.extend(self._get_items_from_categories(chunk))
+
+        return items
 
     def _get_ids(self) -> list[int]:
         """Get all category ids."""
@@ -24,9 +47,59 @@ class Selver(Store):
 
         response = requests.get(url)
         data = response.json()
-        # find the categories
+        # find the base categories
         categories = data["hits"]["hits"][0]["_source"]["children_data"]
-        # extract category ids from the categories
-        ids = [category["id"] for category in categories]
+        # extract sub-categories
+        sub_categories = [category["children_data"] for category in categories]
+        # extract category ids
+        category_ids = [[y["id"] for y in x] for x in sub_categories]
+        # make the 2d list into a 1d list
+        ids = []
+        for i in category_ids:
+            ids.extend(i)
 
         return ids
+
+    def _get_items_from_categories(self, category_ids: list[int] | int) -> list[Item]:
+        """Get all items from a category."""
+        # construct the request json string
+        request = construct_request(category_ids)
+        # construct the url
+        url = (
+            "https://www.selver.ee/api/catalog/vue_storefront_catalog_et/product/_search?request="
+            + request
+            + "&size=1000"
+        )
+
+        response = requests.get(url)
+        data = response.json()
+
+        products = [x["_source"] for x in data["hits"]["hits"]]
+
+        items: list[Item] = []
+        for product in products:
+            price = product["final_price_incl_tax"]
+            base_price = product["unit_price"]
+            quantity = price / base_price
+            unit = " ".join(
+                [
+                    x
+                    for x in product["product_volume"].split(" ")
+                    if x.isdigit() == False
+                ]
+            )
+
+            item = Item(
+                name=product["name"],
+                code=product["product_main_ean"],
+                price=price,
+                unit=unit,
+                category=product["category"][0]["name"],
+                quantity=quantity,
+                url="https://www.selver.ee/" + product["url_path"],
+                base_price=base_price,
+                image="https://www.selver.ee/img/50/50/resize" + product["image"],
+            )
+            items.append(item)
+
+        return items
--- src/tests/test_selver.py
+++ src/tests/test_selver.py
@@ -1,12 +1,18 @@
 """Test selver.py module."""
 
+import json
+import os
+import random
+import requests
 from src.stores.selver.selver import Selver
 from src.stores.selver.helper import construct_request
 
 #########################
 # Setup
-Selver = Selver()
-category_ids = Selver._get_ids()
+selver = Selver()
+category_ids = selver._get_ids()
+products = selver._get_items_from_categories(category_ids[:6])
+all_products = selver.get_items(_test=True)
 
 #########################
 
@@ -23,3 +29,47 @@ def test_construct_request():
         construct_request(209)
         == '{"query": {"bool": {"filter": {"bool": {"must": [{"terms": {"visibility": [2, 3, 4]}}, {"terms": {"status": [0, 1]}}, {"terms": {"category_ids": [209]}}]}}}}}'
     )
+
+
+def test_request_categories():
+    """Test if the request works for one category."""
+    assert len(products) > 0
+    for item in products:
+        assert item["name"] is not None
+        assert item["price"] > 0
+        assert item["unit"] is not None
+        assert item["quantity"] > 0
+        assert item["url"] is not None
+        assert item["base_price"] > 0
+        assert item["image"] is not None
+
+
+def test_item_links_work():
+    """Test if the item links work. image and url"""
+    item = products[0]
+    response = requests.get(item["url"])
+    assert response.status_code == 200
+    response = requests.get(item["image"])
+    assert response.status_code == 200
+
+    item2 = products[random.randint(0, len(products))]
+    response = requests.get(item2["url"])
+    assert response.status_code == 200
+    response = requests.get(item2["image"])
+    assert response.status_code == 200
+
+
+def test_get_all_products():
+    assert len(all_products) > 0
+    for item in all_products:
+        assert item["name"] is not None
+        assert item["price"] > 0
+        assert item["unit"] is not None
+        assert item["quantity"] > 0
+        assert item["url"] is not None
+        assert item["base_price"] > 0
+        assert item["image"] is not None
+    # log the products to a file for manual inspection
+    path = os.path.join(os.path.dirname(__file__), "selver_products.json")
+    with open(path, "w", encoding="UTF-8") as f:
+        f.write(json.dumps(all_products, indent=4))
